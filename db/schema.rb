a1b03db3ab12c33614f423fbf881cf615142c0ec
tharveybrown <tharveybrown@gmail.com>
2020-05-26 12:46:42 -0700

    add login and session storage of user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_172533) do
+ActiveRecord::Schema.define(version: 2020_05_26_190233) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
