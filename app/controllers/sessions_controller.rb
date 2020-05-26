6719f7bc7bb16ba722dbdfce314d7432ee7e3973
tharveybrown <tharveybrown@gmail.com>
2020-05-26 12:51:03 -0700

    bug fix on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:user][:email])
-    @user = user.try(:authenticate, params[:user][:password])
+    @user = @user.try(:authenticate, params[:user][:password])
     return redirect_to(controller: 'sessions', action: 'new') unless @user
 
     session[:user_id] = @user.id
-    redirect_to controller: 'users', action: 'home'
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
